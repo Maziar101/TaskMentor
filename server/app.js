00421a5c33acc38bafbc62fcdc309efbcfa25293
Maziar <maziarzamani85@gmail.com>
2026-01-13 23:46:29 +0330

    Add Task Routes

--- server/app.js
+++ server/app.js
@@ -2,11 +2,13 @@ import express from "express";
 import authRoutes from "./routes/auth.js";
 import notificationRoutes from "./routes/notifications.js";
 import scheduleRoutes from "./routes/schedule.js";
+import taskRoutes from "./routes/tasks.js";
 
 const app = express();
 
 app.use("/api/auth", authRoutes);
 app.use("/api/notifications", notificationRoutes);
 app.use("/api/schedule",scheduleRoutes);
+app.use("/api/tasks",taskRoutes);
 
 export default app;
