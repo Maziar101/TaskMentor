acd6e7d1aa2aaf092e02500f63bf30c54e05a473
Maziar <maziarzamani85@gmail.com>
2026-01-16 19:59:19 +0330

    Add TeamAudit Route

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import authRoutes from "./routes/auth.js";
 import notificationRoutes from "./routes/notifications.js";
 import scheduleRoutes from "./routes/schedule.js";
 import taskRoutes from "./routes/tasks.js";
+import teamAuditRoutes from "./routes/teamAudit.js";
 
 const app = express();
 
@@ -10,5 +11,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/notifications", notificationRoutes);
 app.use("/api/schedule",scheduleRoutes);
 app.use("/api/tasks",taskRoutes);
+app.use("/api/teams/audit",teamAuditRoutes);
+app.use("/api/")
 
 export default app;
