d1f73b09a9a1420bd2bd9d711e8db712435a265d
Maziar <maziarzamani85@gmail.com>
2026-01-17 11:12:14 +0330

    Add TeamMembers Route

--- server/app.js
+++ server/app.js
@@ -5,6 +5,8 @@ import scheduleRoutes from "./routes/schedule.js";
 import taskRoutes from "./routes/tasks.js";
 import teamAuditRoutes from "./routes/teamAudit.js";
 import teamGroupRoutes from "./routes/teamGroups.js";
+import teamsRoutes from "./routes/teams.js";
+import teamMembersRoutes from "./routes/teamMembers.js";
 
 const app = express();
 
@@ -12,7 +14,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/notifications", notificationRoutes);
 app.use("/api/schedule",scheduleRoutes);
 app.use("/api/tasks",taskRoutes);
+app.use("/api/teams",teamsRoutes);
 app.use("/api/teams/audit",teamAuditRoutes);
 app.use("/api/teams/group",teamGroupRoutes);
+app.use("/api/teams/members",teamMembersRoutes);
 
 export default app;
