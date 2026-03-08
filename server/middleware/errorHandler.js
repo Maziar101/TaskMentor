ea7cfaa682e9314eac42c2f763da32c2e3e97157
Maziar <maziarzamani85@gmail.com>
2026-03-08 09:31:11 +0330

    Add Some Codes

--- server/middleware/errorHandler.js
+++ server/middleware/errorHandler.js
@@ -1,7 +1,12 @@
 function errorHandler(err, _req, res, _next) {
   // eslint-disable-next-line no-console
   console.error(err);
-  const status = err.status || 500;
+  const status =
+    Number.isInteger(err?.statusCode) && err.statusCode > 0
+      ? err.statusCode
+      : Number.isInteger(err?.status) && err.status > 0
+      ? err.status
+      : 500;
   const message = err.message || "Unexpected error";
   res.status(status).json({ message });
 }
