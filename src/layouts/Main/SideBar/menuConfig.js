e9b02e8138cf7cebd57c2f0408e78b4eba1a3306
Maziar <maziarzamani85@gmail.com>
2026-09-24 13:18:47 +0330

    fix(navigation): use outline chat icon

--- src/layouts/Main/SideBar/menuConfig.js
+++ src/layouts/Main/SideBar/menuConfig.js
@@ -1,9 +1,8 @@
-import { FiCalendar, FiGrid, FiUser } from "react-icons/fi";
-import { IoMdChatbubbles } from "react-icons/io";
+import { FiCalendar, FiGrid, FiMessageSquare, FiUser } from "react-icons/fi";
 
 export const menuItems = [
     { label: "داشبورد", to: "/dashboard", icon: FiGrid },
     { label: "برنامه‌ریز", to: "/planner", icon: FiCalendar },
-    { label:"چت" , to: "/chat", icon: IoMdChatbubbles },
+    { label:"چت" , to: "/chat", icon: FiMessageSquare },
     { label: "پروفایل", to: "/profile", icon: FiUser },
 ];
